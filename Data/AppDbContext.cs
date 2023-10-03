84491afffa736f15a16ab4ed67ae19b59c01d8bb
RafaelTozzo <tozzo_rlz@hotmail.com>
2023-10-03 14:37:19 -0300

    Here we go again

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<TodoModel> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
